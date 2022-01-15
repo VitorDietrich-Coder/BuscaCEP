b424bdd34456dbfe88fcd0e9585ff21dad1b14d1
Vitor Dietrich <vitordietrich@outlook.com>
2022-01-15 08:17:49 -0300

    comiit

--- BuscaAPI/Form1.Designer.cs
+++ BuscaAPI/Form1.Designer.cs
@@ -48,7 +48,7 @@ namespace BuscaAPI
             // 
             this.button1.Location = new System.Drawing.Point(297, 288);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(107, 43);
+            this.button1.Size = new System.Drawing.Size(136, 43);
             this.button1.TabIndex = 0;
             this.button1.Text = "Busca CEP";
             this.button1.UseVisualStyleBackColor = true;
